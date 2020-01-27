5ed650e5ab966c9752e7376f94473d553693dcd2
Andrei Pivoda <andrei.pivoda.external@autoliv.com>
2020-01-27 16:25:38 +0200

    added login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Button } from "tns-core-modules/ui/button";
 
 @Component({
-  selector: 'ns-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'ns-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+    private inputPIN = "";
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    onTap(args) {
+        let button = args.object as Button;
+        console.log(button.text);
+        this.inputPIN+=button.text;
+    }
 
 }
